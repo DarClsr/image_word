98d004e3a8a14d36257189f661f44d384cc3c9b8
iwan <1322677864@qq.com>
2026-03-16 23:34:43 +0800

    feat: integrate AuthModule and add guards to Ad and System modules
    - Imported AuthModule into AdModule and SystemModule for authentication support.
    - Added ClientAuthGuard to AdModule and AdminAuthGuard to SystemModule to enhance security.

--- server/src/modules/ad/ad.module.ts
+++ server/src/modules/ad/ad.module.ts
@@ -6,10 +6,13 @@ import { AdService } from './ad.service';
 import { AdController } from './ad.controller';
 import { AdminAdController } from './admin-ad.controller';
 import { PrismaService } from '../../shared/prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
+import { ClientAuthGuard } from '../../common/guards';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AdController, AdminAdController],
-  providers: [AdService, PrismaService],
+  providers: [AdService, PrismaService, ClientAuthGuard],
   exports: [AdService],
 })
 export class AdModule {}
--- server/src/modules/system/system.module.ts
+++ server/src/modules/system/system.module.ts
@@ -4,10 +4,13 @@
 import { Module } from '@nestjs/common';
 import { SystemService } from './system.service';
 import { AdminSystemController } from './controllers/admin-system.controller';
+import { AuthModule } from '../auth/auth.module';
+import { AdminAuthGuard } from '../../common/guards';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AdminSystemController],
-  providers: [SystemService],
+  providers: [SystemService, AdminAuthGuard],
   exports: [SystemService],
 })
 export class SystemModule {}
